c1bf8dc92c2917ea621ea6eb8de39006765d953b
Damian <dodendaa@student.wethinkcode.co.za>
2019-10-20 18:14:54 +0200

    setting up header file

---
+++ server/header.php
@@ -0,0 +1,25 @@
+// header here bruh 
+
+
+<html>
+	<head>
+
+	</head>
+	<body>
+	       <div class = "header">
+                <h1 style = "margin-top:1%;">Camagru</h1>
+                <h4><a href = "login.php">Login</a> &nbsp <a href = "register.php">Register</a></h4>
+            </div>
+
+        <div class = "menu_bar">
+            <ul>
+                <li class = "active" id = "login"><a href = "index.php">Home</a></li>
+                <li><a href = "">Sign Up</a></li>
+                <li><a href = "">Products</a>
+                <li><a href = "">My Account</a>
+      <!--         <li><a href = "https://web.whatsapp.com/">Contact us</a></li>    -->
+            </ul>
+        </div>
+	</body>
+
+</html>
