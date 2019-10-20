586c969f1d25801847778fa2ca8329e87f614497
Damian <dodendaa@student.wethinkcode.co.za>
2019-10-20 18:18:49 +0200

    setting up login and forgot password file

---
+++ server/login.php
@@ -0,0 +1,19 @@
+// header here
+
+
+<html>
+	<head>
+	</head>
+	<body>
+      <center><form>
+           <p>Username: <input type="text" name="login"/></p>
+           <p>Password: <input  type="password" name="passwd"/></p>
+           <a class = "submit"  href = "">submit</a>                                 
+		   <br/><br/><br/>
+			<a href = "register.php">Don't have an account?  Sign Up now</a><br/>
+			<a href = "forgot_password.php">Forgot your Password?</a>
+
+       </form></center>
+   </body>
+
+</html>
