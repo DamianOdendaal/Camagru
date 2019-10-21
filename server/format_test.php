19155414935bd507cf29a1d9d02a0daab8056af8
Damian ODENDAAL <dodendaa@c5r9s4.wethinkcode.co.za>
2019-10-21 15:48:47 +0200

    work

--- server/format_test.php
+++ server/format_test.php
@@ -26,7 +26,7 @@
   padding:0 20px;
 }
 .right {
-  background-color:#e5e5e5;
+  background-color:#1EE2E2;
   float:right;
   width:20%;
   padding:15px;
@@ -40,7 +40,7 @@ h1 {
 }
 body {
     font-family:Verdana;
-    background-image: url('https://images.unsplash.com/photo-1452587925148-ce544e77e70d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80');
+    background-image: url('https://images.unsplash.com/photo-1500521680613-a8f77c5cd0f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80');
     background-repeat: no-repeat;
     background-size:cover;
     /* background-position:center; */
@@ -56,7 +56,7 @@ body {
 </head>
 <body>
 
-<div style="background-color:#B6A320; padding:15px; text-align:center; ">
+<div style="background-color:#1EE2E2; padding:15px; text-align:center; ">
   <h1>Camagru</h1>
 </div><br/>
 
@@ -68,17 +68,17 @@ body {
   </div>
 
   <div class="right">
-    <h2>My Gallery</h2><br/>
-    <div class="menu">
-        <a href="#">Picture 1</a>
-        <a href="#">Picture 2</a>
-        <a href="#">Picture 3</a>
-        <a href="#">Picture 4</a>
-    </div>
+    <div class = ""><h2>My Gallery</h2><br/>
+        <div class="menu">
+            <a href="#">Picture 1</a>
+            <a href="#">Picture 2</a>
+            <a href="#">Picture 3</a>
+            <a href="#">Picture 4</a>
+        </div>
   </div>
 </div>
 
-<div style="background-color:#e5e5e5; text-align:center; padding:10px; margin-bottom:0%;">© copyright Camagru.com</div>  <!-- fix this margin to work with the bottom-->
+<div style="background-color:#1EE2E2; text-align:center; padding:10px; margin-bottom:0%;">© copyright Camagru.com</div>  <!-- fix this margin to work with the bottom-->
 
 </body>
 </html>
