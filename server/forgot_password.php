c8905aa79156836234358919bc027f09a53f3dbe
Damian ODENDAAL <dodendaa@c5r9s4.wethinkcode.co.za>
2019-10-22 08:17:07 +0200

    fixing format

--- server/forgot_password.php
+++ server/forgot_password.php
@@ -4,20 +4,42 @@
 
 // add a bg image
 
+<!-- header here -->
 <html>
 	<head>
+		<style>
+		body, html {
+			height: 100%;
+		}
+			.image {
+				height: 50%;
+				width: 40%;
+				display: block;
+ 				margin-left: auto;
+  				margin-right: auto;
+			}
+			form {
+				margin-top: 40%;
+				text-align: center;
+			}
+			body {
+				background: url('https://images.unsplash.com/photo-1506638794690-bbea5c6f0ab7?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80') no-repeat;
+				height:100%;
+				background-size:cover;
+			}
+		</style>
 	</head>
 	<body>
-		<div class = "f_password">
-			<h2>Forgot your password?</h2><br/>
-			<p>Please enter your email address.</p>
-			<p>a reset password will be sent to your entered email address</p><br/>
-			<form>
-           		<p><input type="text" name="f_pass_entered"/></p><br/>
-				<button>Send</button>
-				<!-- when they press send it must go to a new page where they must enter a number thats given in the email-->
-			</form>
+		<div class="sign">
+  			<form action="" method="POST">
+    			<fieldset>
+			<legend>Forgot your password ?</legend>
+      				<label for="name">Please enter your <u>Email</u>.<br>A reset password link<br>will be sent to your<br>email address.<br><br></label><input type="email" name="username" maxlength="64" required><br>
+    			</fieldset>
+    				<p style="text-align: center;"><input class="submit-btm" type="submit" value="Send"></p>
+  			</form>
 		</div>
-   </body>
+	</body>
 
 </html>
+
