b89d1950ffee286e7180f4d0067cc53129389e1c
damianodendaal <dodendaa@student.wethinkcode.co.za>
2019-10-29 11:34:16 +0200

    power is being funny

--- posting.php
+++ posting.php
@@ -1,10 +1,25 @@
-// so first create a database for the pictures
-// then create a table
-// watch the video to finish this property_exists
-// TO DO FOR THE DAY!!
-1.) finish posting
-2.) make sure you are able to do print memory, options and brackets before you leave school
-3.) try to see how much you can do in terms of the likes and comments
-4.) see how you can hash passwords and thenn learn how to mask them
-5.) learn properly how to communicate with the database
-// remember to recreate the files you made
+<html lang="en">
+    <head>
+        <style>
+                    
+       </style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="content">
+            <form method = "post" action="posting.php" enctype = "multipart/form-data">
+                <input type="hidden" name = "size" value="1000000">
+                    <div>
+                        <input type="file" name = "image">
+                    </div>
+                    <div>
+                        <textarea name="text" placeholder = "Put a story behind your picture" cols="40" rows="3"></textarea>
+                    </div>
+                    <div>
+                        <input type="submit" name = "upload" value = "upload_image">
+                    </div>
+            </form>
+        </div>
+    </body>
+</html> 
