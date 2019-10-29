11f8f17bf7862f712960fe92208c1032fad3bb13
damianodendaal <dodendaa@student.wethinkcode.co.za>
2019-10-29 09:15:45 +0200

    pooooooosts

---
+++ posting.php
@@ -0,0 +1,10 @@
+// so first create a database for the pictures
+// then create a table
+// watch the video to finish this property_exists
+// TO DO FOR THE DAY!!
+1.) finish posting
+2.) make sure you are able to do print memory, options and brackets before you leave school
+3.) try to see how much you can do in terms of the likes and comments
+4.) see how you can hash passwords and thenn learn how to mask them
+5.) learn properly how to communicate with the database
+// remember to recreate the files you made
