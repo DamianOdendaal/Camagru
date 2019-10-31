3f3289dcacc4b394f6bb310d9c0ec20135a12748
damianodendaal <dodendaa@student.wethinkcode.co.za>
2019-10-31 16:01:33 +0200

    fixing the posting part of life

--- posting.php
+++ posting.php
@@ -4,14 +4,14 @@
 
     // condition to check if the submit button was pressed, if its pressed we give it the path to store the uploaded pic
     if (isset($_POST['upload'])){
-        $target = "images/".basename($_FILES['image']['name']);
+        $target = "images/".basename($_FILES['content']['name']);
 
         // connect to the database
         $connect = new PDO($DB_SERVER, $DB_USER, $DB_PASSWORD);                                                                     //come here and add the database name
 
         // get all of the data that was submitted from the form
 
-        $images = $_FILES['image']['name'];
+        $images = $_FILES['content']['name'];
         $text = $_POST['text'];
 
         $sql = "INSERT INTO images (image, text) VALUES ('$images', '$text')";
@@ -19,7 +19,7 @@
         $connect->exec($sql); // this stores the values into the database table (images)
 
 	// now we move the images to the images dir
-	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)){ 				//try tmp_name if you get any bugs
+	if (move_uploaded_file($_FILES['content']['tmp_name'], $target)){ 				//try tmp_name if you get any bugs
 		$message = "Image has successfully uploaded";
     }
 	else {
@@ -70,11 +70,11 @@
         <div id="content">
 	<!-- displaying the images from the database -->
 	<?php
-        $connect = new PDO($first_param, $name, $password);
-		$sqll = 'SELECT * FROM images';
+        $connect = new PDO($DB_SERVER, $DB_USER, $DB_PASSWORD);
+		$sql = 'SELECT * FROM images';
 		while ($pic = $connect->query($sql))
 			echo "<div id = 'img_div'>";                      // if bugs look here first
-				echo "<img src = 'images/".$pic['image']."'>";
+				echo "<img src = 'images/".$pic['content']."'>";
 				echo "<p>".$pic['text']."</p>";
 			echo "</div>";
 		}											// if you come across any bugs try to change this back to sql 
@@ -90,7 +90,7 @@
                         <textarea name="text" placeholder = "Put a story behind your picture" cols="40" rows="3"></textarea>
                     </div>
                     <div>
-                        <input type="submit" name = "upload" value = "upload_image">
+                        <input type="submit" name = "upload" value = "Upload Image">
                     </div>
             </form>
         </div>
