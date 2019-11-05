76fe62d8513ac0c6882a66bc76751f34e96bf3d8
damianodendaal <dodendaa@student.wethinkcode.co.za>
2019-11-05 15:21:19 +0200

    seeing if you are logged in

--- success_verified.php
+++ success_verified.php
@@ -1,5 +1,5 @@
-<!-- <?php
+<?php
     include ("connect.php");
     $username = $_POST['username'];
     $DB_NAME->query("UPDATE `users` SET `status` = 'active' WHERE `username` = '$username';");
-?> -->
+?>
