34823a51d8d32e7b63e848aca994961f8581014a
damianodendaal <dodendaa@student.wethinkcode.co.za>
2019-11-05 15:22:39 +0200

    javascript function to populate a table with images

--- into_table.js
+++ into_table.js
@@ -1,15 +1,15 @@
-// function intoTable(){
-//     var x = document.createElement("TABLE");
-//     x.setAttribute("id", "myTable");
-//     document.body.appendChild(x);
+function intoTable(){
+    var x = document.createElement("TABLE");
+    x.setAttribute("id", "myTable");
+    document.body.appendChild(x);
   
-//     var y = document.createElement("TR");
-//     y.setAttribute("id", "myTr");
-//     document.getElementById("myTable").appendChild(y);
+    var y = document.createElement("TR");
+    y.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(y);
   
-//     var z = document.createElement("TD");
-//     var t = document.createElement("IMG");
-//     t.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsGd_YwYO_jSZuhD5Ll4XI8tYz-IRJuAsc9XhBe34HYjUG_Uey&s");
-//     z.appendChild(t);
-//     document.getElementById("myTr").appendChild(z);
-// }
+    var z = document.createElement("TD");
+    var t = document.createElement("IMG");
+    t.setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsGd_YwYO_jSZuhD5Ll4XI8tYz-IRJuAsc9XhBe34HYjUG_Uey&s");
+    z.appendChild(t);
+    document.getElementById("myTr").appendChild(z);
+}
