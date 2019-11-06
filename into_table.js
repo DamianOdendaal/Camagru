06f20ae6ca388e2a78a4305ba3212afba76a6c51
damianodendaal <dodendaa@student.wethinkcode.co.za>
2019-11-06 09:06:19 +0200

    fixing into_table

--- into_table.js
+++ into_table.js
@@ -1,15 +1,13 @@
 function intoTable($image_path){
     var table = document.createElement("TABLE");
-    table.setAttribute("id", "myTable");
     document.body.appendChild(table);
   
     var row = document.createElement("TR");
-    row.setAttribute("id", "myTr");
     document.getElementById("myTable").appendChild(row);
   
     var data = document.createElement("TD");
     var image = document.createElement("IMG");
     image.setAttribute("src", $image_path);
     data.appendChild(image);
-    document.getElementById("myTr").appendChild(data);
+    row.appendChild(data);
 }
