7c792b74907d48d6fb212f8a4f335030f0e43e05
damianodendaal <dodendaa@student.wethinkcode.co.za>
2019-11-11 12:26:54 +0200

    scrapping the js filters idea and doing it i php

--- video.js
+++ video.js
@@ -3,12 +3,12 @@
     captureButton.addEventListener("click", onCapture);	
     var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
-    svg = document.getElementById('svg'),  
-    star = document.getElementById('star'),
-    smart = document.getElementById('smart'),
-    crown = document.getElementById('crown'),                
     video = document.getElementById('video');
-    star.addEventListener("mouseover", Stickers);
+    // svg = document.getElementById('svg'),  
+    // star = document.getElementById('star'),
+    // smart = document.getElementById('smart'),
+    // crown = document.getElementById('crown'),                
+    // star.addEventListener("mouseover", Stickers);
 
     // vendorURL = window.URL || window.webkitURL;	
 
@@ -32,23 +32,18 @@
         // console.log(canvas.toDataURL());
     }
     
-    function Stickers() {
-        // context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        video.play();
-        context.drawImage(svg, 0, 0, svg.width , svg.height);
-    }
-    function drawPattern(){
-        var svgns = "http://www.w3.org/2000/svg",
-        container = document.getElementById( 'cont' );
-        for (var x = 0; x < 500; x += 20) {
-            for (var y = 0; y < 300; y += 50) {
-               var circle = document.createElementNS(svgns, 'circle');
-               circle.setAttributeNS(null, 'cx', x);
-               circle.setAttributeNS(null, 'cy', y);
-               circle.setAttributeNS(null, 'r', 50);
-               circle.setAttributeNS(null, 'style', 'fill: none; stroke: purple; stroke-width: 1px; opacity: 1;' );
-               container.appendChild(circle);
-            }
-        }
-    }
+    // function drawPattern(){
+    //     var svgns = "http://www.w3.org/2000/svg",
+    //     container = document.getElementById( 'cont' );
+    //     for (var x = 0; x < 500; x += 20) {
+    //         for (var y = 0; y < 300; y += 50) {
+    //            var circle = document.createElementNS(svgns, 'circle');
+    //            circle.setAttributeNS(null, 'cx', x);
+    //            circle.setAttributeNS(null, 'cy', y);
+    //            circle.setAttributeNS(null, 'r', 50);
+    //            circle.setAttributeNS(null, 'style', 'fill: none; stroke: purple; stroke-width: 1px; opacity: 1;' );
+    //            container.appendChild(circle);
+    //         }
+    //     }
+    // }
 })();
