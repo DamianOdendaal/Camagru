61e9917c661ed57ee464580c6395e961fbce7057
damianodendaal <dodendaa@student.wethinkcode.co.za>
2019-11-14 16:29:11 +0200

    gonna continue working at home

--- success_verified.php
+++ success_verified.php
@@ -2,5 +2,7 @@
     include ("connect.php");
     $username = $_POST['username'];
     $DB_NAME->query("UPDATE `users` SET `status` = 'active' WHERE `username` = '$username';");
-    header("location: success_verified.php");
+    header("location: success_verified.php");           // actually make a button that they will press that will redirect them to the index page
+
+    // make sure the send mail works and see if you can do a sql injection
 ?>
